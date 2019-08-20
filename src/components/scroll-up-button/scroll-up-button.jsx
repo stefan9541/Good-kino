1f8091504a65e7a8156f3575dc0084ac5c3a2482
Stefan9541 <stepi08@rambler.ru>
2019-08-20 23:17:36 +0300

    privet

--- src/components/scroll-up-button/scroll-up-button.jsx
+++ src/components/scroll-up-button/scroll-up-button.jsx
@@ -5,6 +5,7 @@ import "./scroll-up-button.css"
 
 const ScrollUpButtonComponent = () => {
   console.log("privet git)")
+  console.log("privet git)")
   return (
     <BackTop id="back-to-top-button" />
   );
