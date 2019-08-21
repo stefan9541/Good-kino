38c270522cf595fb795c1e6a03b51520c4cd42d8
Stefan9541 <stepi08@rambler.ru>
2019-08-21 14:41:11 +0300

    add video control panel

--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import {Icon} from "antd"
 
 
 // const videoRef = React.createRef();
+import "./video.css"
 
-
-const Video = (props) => {
+const Video = React.forwardRef((props, ref) => {
   const {
     getCurrentTime,
     tooglePlayVideo,
     hidePreviousPlayButton,
     onLoadVisibleBlock,
-    videoRef
   } = props
   return (
     <div className="video-wrapp">
@@ -19,11 +19,11 @@ const Video = (props) => {
           <Icon type="play-circle" />
         </button>
       </div>
-      <video onTimeUpdate={getCurrentTime} onClick={tooglePlayVideo} ref={videoRef} >
+      <video onTimeUpdate={getCurrentTime} onClick={tooglePlayVideo} ref={ref} >
         <source type="video/mp4" src={'/multimedia/tony-tone.mp4'} />
       </video >
     </div>
   );
-}
+})
 
 export default Video;
