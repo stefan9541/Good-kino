a130b9596edc0d6a3a5e3090aafce2567576aea8
Stefan9541 <stepi08@rambler.ru>
2019-09-27 16:54:32 +0300

    server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const config = require("./config");
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
