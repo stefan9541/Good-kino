9cde60f78d8eb2af8b2b8637ce2cbdba2ec2008c
stefan9541 <45336682+stefan9541@users.noreply.github.com>
2019-11-05 16:45:20 +0300

    Delete config.js

--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  port: "8080",
-  mongoUri: "mongodb+srv://Stepan:2h8RZMxRYyKynSlp@cluster0-qvftd.gcp.mongodb.net/test?retryWrites=true&w=majority&ssl=true&authSource=admin"
-};
